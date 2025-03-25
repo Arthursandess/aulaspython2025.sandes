822e75e4e7dba150e36bae634a0c6de5dfcef2aa
labsfiap <arthur.sandesnunes@gmail.com>
2025-03-24 21:22:08 -0300

    AULA 2

--- AULA_02_EMR.py
+++ AULA_02_EMR.py
@@ -11,7 +11,70 @@ print(2*A*3*B)
 #EXERCICIO 3
 num1 = 1
 num2 = 2
-num3 = 3
+num3 = 2.16666666
 soma= num1+num2+num3
 
 print(soma)
+#EXEMPLO
+print (type(num1))
+print (type(num3))
+
+#EXEMPLO2
+
+print (f"soma= {soma:.2f}")
+
+#EXEMPLO3
+d = True
+
+print (type(d))
+nome="Arthur Sandes"
+
+print(type(nome))
+
+#EXEMPLO4
+
+print(10%3)
+
+print(10//3)
+
+print(10/3)
+
+print(10**3)
+#EXEMPLO5
+
+nota=8
+media=7
+
+aprovado= nota>= media
+
+print (aprovado)
+
+#EXEMPLO6
+
+nome_comp = input("Digite seu nome completo, por favor:")
+idade = int(input("Idade: "))
+altura=float(input("Altura: "))
+peso=float(input("Peso: "))
+print(nome_comp)
+print(idade)
+print(altura)
+print(peso)
+
+#EXEMPLO7
+
+anos=int(input("Anos de serviço: "))
+
+valor_por_ano=float(input("Valor por ano: "))
+
+bonus=anos*valor_por_ano
+
+print("Bonus de R$", bonus)
+
+#Exercicio4
+
+A=int(input("IDADE DO SEU PAI: "))
+
+B=int(input("IDADE DA SUA MÃE: "))
+
+print(A)
+print(B)
