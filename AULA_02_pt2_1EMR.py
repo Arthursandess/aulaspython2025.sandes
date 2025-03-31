fd23cd0eae4ca7304fd4165f0cfe4df6c4714541
labsfiap <arthur.sandesnunes@gmail.com>
2025-03-31 20:49:12 -0300

    Aula 3 PYTHON

---
+++ AULA_02_pt2_1EMR.py
@@ -0,0 +1,33 @@
+# #Exercicio4
+#
+# A=int(input("IDADE DO SEU PAI: "))
+#
+# B=int(input("IDADE DA SUA MÃE: "))
+#
+# soma=A+B
+#
+# print(A)
+# print(B)
+# print(soma)
+
+#EXERCICIO 5
+
+# M =int(input("METROS: "))
+#
+# mm= M/1000
+#
+# print("metros em mm =")
+# print (mm)
+
+#EXERCICIO 6
+dias=int(input("Dias: "))
+horas = int(input("Horas: "))
+mins = int(input("Minutos: "))
+segs = int(input("Segundos: "))
+
+uso = dias*1440 +horas*360 + mins*60 + segs
+
+print("tempo de uso = ")
+
+print (uso)
+
--- AULA_03_1EMR.py
+++ AULA_03_1EMR.py
@@ -1,33 +1,50 @@
-# #Exercicio4
-#
-# A=int(input("IDADE DO SEU PAI: "))
-#
-# B=int(input("IDADE DA SUA MÃE: "))
-#
-# soma=A+B
-#
-# print(A)
-# print(B)
-# print(soma)
+#TERCEIRA AULA DE PYTHON|CONTEÚDO: IF + ELSE
 
-#EXERCICIO 5
+media_final = float(input("Média final de 0 a 10= "))
+if media_final >= 6:
+    print("Você está APROVADO!!")
+else:
+    print("Você foi REPROVADO!!")
 
-# M =int(input("METROS: "))
-#
-# mm= M/1000
-#
-# print("metros em mm =")
-# print (mm)
+ # EXERCICIO 1:
+vel_max = int(input("Velocidade ao passar no radar = "))
 
-#EXERCICIO 6
-dias=int(input("Dias: "))
-horas = int(input("Horas: "))
-mins = int(input("Minutos: "))
-segs = int(input("Segundos: "))
+if vel_max > 80:
+    multa =(vel_max - 80) * 5
+    print("VOCE FOI MULTADO EM R$ %.2f" %multa)
+else:
+    print("OK!")
+# EXERCICIO 2:
 
-uso = dias*1440 +horas*360 + mins*60 + segs
+num_1 = float(input("Nº1 ="))
+num_2 = float(input("Nº2 ="))
+num_3 = float(input("Nº3 ="))
 
-print("tempo de uso = ")
+if num_1>num_2 and num_1>num_3:
+        print(f"o maior numero é: {num_1}")
+if num_2>num_3 and num_2>num_1:
+     print(f"o maior numero é: {num_2}")
+if num_3>num_2 and num_3>num_1:
+     print(f"o maior numero é: {num_3}")
 
-print (uso)
+if num_1<num_2 and num_1<num_3:
+        print(f"o menor numero é: {num_1}")
+if num_2<num_3 and num_2<num_1:
+     print(f"o menor numero é: {num_2}")
+if num_3<num_2 and num_3<num_1:
+     print(f"o menor numero é: {num_3}")
+
+# EXERCICIO 3
+
+salario= float(input("Valor do seu salário atual: "))
+
+if salario >1250:
+
+    novo_salario =salario + salario*0.1
+    print("Seu novo salário é R$ %.2f" %novo_salario)
+
+if salario <1250:
+
+    novo_salario =salario + salario*0.15
+    print("Seu novo salário é R$ %.2f" %novo_salario)
 
