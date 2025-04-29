4839ad5bf0dc2faaee489fd9e3c9d64a443c4ee2
arthur.samndes <arthur.sandesnunes@gmail.com>
2025-04-28 21:01:08 -0300

    aula 4

---
+++ AULA_04_1EMR.py
@@ -0,0 +1,70 @@
+#aula 4 de python
+
+#num1 = float(input("Digite o primeiro numero para operação: "))
+#num2 = float(input("Digite o segundo numero para operação: "))
+#operacao = input("Qual será a operação a ser realizada:")
+
+#if operacao == "+":
+#    result = num1 + num2
+#elif operacao == "-":
+#   result = num1 - num2
+#elif operacao == "*":
+#    result = num1 * num2
+#elif operacao == "/":
+#    result = num1 / num2
+#else:
+#    print("Numero ou/e Operador invalido, por favor, tente novamente.")
+
+#print(f"O resultado da sua operação é:{result:.2f}")
+
+#n1 = float(input("Digite a primeira nota: ")
+#n2 = float(input("Digite a segunda nota: ")
+
+#cal = (n1 + n2) / 2
+
+#if cal >= 6:
+ #   print("O aluno está aprovado!")
+#else:
+   # print("O aluno está reprovado!")
+
+
+
+
+
+#EXERCICIO 1
+#x=1
+#while x <=100:
+#    print(x)
+#    x = x + 1
+
+#EXERCICIO 2
+
+#x=50
+#while x <=100:
+#    print(x)
+#    x = x + 1
+
+#EXERCICIO 3
+
+#x = 10
+#while x >= 0:
+#   print(x)
+#   x = x - 1
+#print("Fogo!")
+
+#EXERCICIO 4
+
+#fim = int(input("Insira o ultimo numero desejado: "))
+#x = 1
+#while x <= fim:
+##    print(x)
+#    x += 2
+
+
+#EXERCICIO 5
+
+#x = 3
+#while x <= 30:
+#    print(x)
+#    x = x + 3
+
